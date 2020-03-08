bef0150e71162ed58dc4441e36bcae87bd195a83
rizialdi <rasselio.diack@gmail.com>
2020-03-08 04:42:03 +0100

    Replaced Apollo-server by graphql-yoga

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import { GraphQLServer } from 'graphql-yoga'
 import { typeDefs, resolvers } from './graphQL'
 import cors from 'cors'
 import { prisma } from './prisma/generated/prisma-client'
 
-const server = new ApolloServer({
+const graphqlserver = new GraphQLServer({
   typeDefs,
   resolvers,
   context: request => ({
     ...request,
     prisma
-  }),
-  introspection: true,
-  playground: !process.env.APP_ENV === 'prod'
+  })
 })
 
-const app = express()
 // TODO: to remove when in production
-app.use(cors())
+graphqlserver.use(cors())
 
-app.disable('x-powered-by')
-app.get('/', function (req, res) {
-  res.send('hello world baby')
-})
+graphqlserver.express.disable('x-powered-by')
 
-server.applyMiddleware({ app })
-
-app.listen({ port: 4000 }, () =>
+graphqlserver.start({ port: 4000 }, () =>
   console.log('🚀 Server ready at http://localhost:4000')
 )
