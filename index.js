110ed1038bf83f895e3546563eb6c216b479f06d
rizialdi <rasselio.diack@gmail.com>
2020-03-08 20:04:24 +0100

    Implemented 2FA with MessageBird api

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import { GraphQLServer } from 'graphql-yoga'
 import { typeDefs, resolvers } from './graphQL'
-import cors from 'cors'
 import { prisma } from './prisma/generated/prisma-client'
+require('custom-env').env('prod')
+
+const { MESSAGEBIRD_API_KEY } = process.env
+const messagebird = require('messagebird')(MESSAGEBIRD_API_KEY)
 
 const graphqlserver = new GraphQLServer({
   typeDefs,
@@ -13,9 +19,50 @@ const graphqlserver = new GraphQLServer({
 })
 
 // TODO: to remove when in production
-graphqlserver.use(cors())
+graphqlserver.express.use(cors())
+// parse application/x-www-form-urlencoded
+graphqlserver.express.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+graphqlserver.express.use(bodyParser.json())
 
 graphqlserver.express.disable('x-powered-by')
+
+graphqlserver.express.post('/api/v1/enregistrement', (req, res) => {
+  const { nom, prenom, numero } = req.body
+  messagebird.messages.create({
+    originator: 'Yoko App',
+    recipients: [numero],
+    body: `Ton nom ${nom} et prenom ${prenom}`
+  }, (error, response) => {
+    if (error) return res.send(error)
+    return res.send(response)
+  })
+})
+
+graphqlserver.express.post('/api/v1/register-step1', (req, res) => {
+  const { numero } = req.body
+  messagebird.verify
+    .create(
+      numero, {
+        originator: 'Yoko App',
+        template: 'Votre code de vérification est %token'
+      }, (error, response) => {
+        if (error) return res.send(error)
+        return res.send(response)
+      })
+})
+
+graphqlserver.express.post('/api/v1/register-step2', (req, res) => {
+  const { id, token } = req.body
+  messagebird.verify
+    .verify(
+      id,
+      token,
+      (error, response) => {
+        if (error) return res.send(error)
+        return res.send(response)
+      })
+})
 
 graphqlserver.start({ port: 4000 }, () =>
   console.log('🚀 Server ready at http://localhost:4000')
