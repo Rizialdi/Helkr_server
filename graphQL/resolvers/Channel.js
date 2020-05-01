045d322166f2a718d7939fdca337951d9516f9f4
rizialdi <rasselio.diack@gmail.com>
2020-05-01 02:30:54 +0200

    Updated message resolver and some refractoring

--- graphQL/resolvers/Channel.js
+++ graphQL/resolvers/Channel.js
@@ -1,63 +1,81 @@
-import { getUserId } from './utils'
+import { getUserId } from './utils';
 
 export default {
   Query: {
-    channel: async (parent, { id }, context, info) => {
-      const channel = await context.prisma.channel.findOne({ where: { id }})
-      if (!channel) return new Error('Chaine inexistante')
-      return channel
+    channel: async (_, { id }, context, __) => {
+      const channel = await context.prisma.channel.findOne({ where: { id } });
+      if (!channel) return new Error('Chaine inexistante');
+      return channel;
     },
-    channels: async (parent, args, context, info) => {
-      return context.prisma.channel.findMany()
+    channels: async (_, __, context, ___) => {
+      return context.prisma.channel.findMany();
     },
-    recipientChannels: async (parent, args, context, info) => {
-      const userId = getUserId(context)
-      const channels = await context.prisma.channel.findMany({ where: { users: { some: { id: userId }} } })
-      const channelIds = channels.map(channel => channel.id)
-      const users = await context.prisma.user.findMany({ where: { AND: [{ channels: { some: { id: { in: channelIds } }} }, { id: { not: userId } }] } })
-      const mapping = []
+    recipientChannels: async (_, __, context, ___) => {
+      const userId = getUserId(context);
+      const channels = await context.prisma.channel.findMany({
+        where: { users: { some: { id: userId } } }
+      });
+      const channelIds = channels.map((channel) => channel.id);
+      const users = await context.prisma.user.findMany({
+        where: {
+          AND: [
+            { channels: { some: { id: { in: channelIds } } } },
+            { id: { not: userId } }
+          ]
+        }
+      });
+      const mapping = [];
       for (var i = 0; i < channelIds.length; i++) {
-        const messages = await context.prisma.message.findMany({ where: { channel: { id: channelIds[i] } } })
-        const lastMessage = messages[messages.length - 1]
-        mapping.push(lastMessage)
+        const messages = await context.prisma.message.findMany({
+          where: { channel: { id: channelIds[i] } }
+        });
+        const lastMessage = messages[messages.length - 1];
+        mapping.push(lastMessage);
       }
-      const lastMessages = JSON.stringify(mapping)
+      const lastMessages = JSON.stringify(mapping);
       return {
         users,
         channelIds,
         lastMessages
-      }
+      };
     }
   },
   Mutation: {
-    createChannel: async (parent, { recipient }, context, info) => {
+    createChannel: async (_, { recipient }, context, __) => {
       try {
-        const sentBy = getUserId(context)
-        const channel = await context.prisma.channel.create({ data: { users: { connect: [{ id: recipient }, { id: sentBy }] } }})
+        const sentBy = getUserId(context);
+        const channel = await context.prisma.channel.create({
+          data: { users: { connect: [{ id: recipient }, { id: sentBy }] } }
+        });
 
         if (!channel) {
           return {
             success: false,
             channel
-          }
+          };
         }
         return {
           success: true,
           channel
-        }
+        };
       } catch (error) {
-        throw new Error('La creation de chaine a echoue')
+        throw new Error('La creation de chaine a echoue');
       }
     }
   },
   Channel: {
     messages: async (parent, _, { prisma }) => {
-      const messages = await prisma.message.findMany({ orderBy: {createdAt: "desc"}, where: { channel: { id: parent.id } } })
-      return messages
+      const messages = await prisma.message.findMany({
+        orderBy: { createdAt: 'desc' },
+        where: { channel: { id: parent.id } }
+      });
+      return messages;
     },
     users: async (parent, _, { prisma }) => {
-      const users = await prisma.user.findMany({ where: { channels: { some: { id: parent.id }} } })
-      return users
+      const users = await prisma.user.findMany({
+        where: { channels: { some: { id: parent.id } } }
+      });
+      return users;
     }
   }
-}
+};
--- graphQL/resolvers/Offering.js
+++ graphQL/resolvers/Offering.js
@@ -1,30 +1,44 @@
-import { getUserId } from './utils'
+import { getUserId } from './utils';
 
 export default {
   Query: {
-    offeringsUser: async (parent, { numero }, context, info) => {
-      const offerings = await context.prisma.user.findOne({ where: { numero }}).offerings()
-      return offerings
+    offeringsUser: async (_, { numero }, context, __) => {
+      const offerings = await context.prisma.user
+        .findOne({ where: { numero } })
+        .offerings();
+      return offerings;
     },
     offerings: async (_, __, context) => {
-      return context.prisma.offering.findMany()
+      return context.prisma.offering.findMany();
     }
   },
   Mutation: {
     addOffering: async (_, { type, category, description }, context) => {
-      const userId = getUserId(context)
-      const offering = await context.prisma.offering.create({ data: { type, category, description, author: { connect: { id: userId } } }})
-      return offering
+      const userId = getUserId(context);
+      const offering = await context.prisma.offering.create({
+        data: {
+          type,
+          category,
+          description,
+          author: { connect: { id: userId } }
+        }
+      });
+      return offering;
     },
     updateOffering: async (_, { id, description }, context) => {
-      const offering = await context.prisma.offering.update({ where: { id }, data: { description }})
-      return offering
+      const offering = await context.prisma.offering.update({
+        where: { id },
+        data: { description }
+      });
+      return offering;
     }
   },
   Offering: {
     author: async (parent, _, { prisma }) => {
-      const author = await prisma.offering.findOne({ where: { id: parent.id }}).author()
-      return author
+      const author = await prisma.offering
+        .findOne({ where: { id: parent.id } })
+        .author();
+      return author;
     }
   }
-}
+};
